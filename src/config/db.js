369e1c3958e000f1961a92d919b7001905c5676b
keshava <167961467+sahasrakeshava@users.noreply.github.com>
2025-02-21 21:48:05 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mondbUrl =
-  "mongodb://172.29.80.192:27017";
+  "mongodb+srv://kaveri:6302747382@cluster0.db24r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDb = () => {
   return mongoose.connect(mondbUrl);
